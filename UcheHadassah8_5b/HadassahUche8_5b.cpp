b57e845b98bd1935d25a0db708fb98cecffd6be6
Hadassah Uche <uhadassah1@gmail.com>
2023-11-28 17:25:12 +0200

    Add project files.

---
+++ UcheHadassah8_5b/HadassahUche8_5b.cpp
@@ -0,0 +1,3 @@
+/*b) Re-write the above exercise with a function: Write a function called IsPalindrome 
+which takes as a parameter a string and returns true (i.e. 1) if the string is a 
+palindrome. Write a main program to test your function with a user-defined string. */
